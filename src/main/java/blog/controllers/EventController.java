a54ff79be44dbb1c3cbdb8e62a312f310a7b7448
Lenny <valentin805@gmail.com>
2018-07-12 11:23:45 +0300

    Fix 'events' table & add search bar.

--- src/main/java/blog/controllers/EventController.java
+++ src/main/java/blog/controllers/EventController.java
@@ -3,10 +3,12 @@ package blog.controllers;
 import blog.entities.Event;
 import blog.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -25,7 +27,8 @@ public class EventController {
 
     @GetMapping("/showEvents")
     public String displayEvents(Model model) {
-        List<Event> events = this.eventRepository.findAll();
+
+        List<Event> events = this.eventRepository.findTop7ByOrderByDateDesc();
         model.addAttribute("events", events);
         return "Events/eventDisplay";
     }
